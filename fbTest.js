e0528f4dad99ff4b636d6e7ad4fd69ff9afc17c1
Geoffrey <fasor@outlook.com>
2015-06-09 10:40:22 +1000

    Facebook Add
    Managed to extract data from the feed given an access token. Still
    haven't done the authenticating. This would still work if we get the
    access token manually from the user...

--- fbTest.js
+++ fbTest.js
@@ -1,25 +1,41 @@
 var FB = require('fb');
 var fs = require('fs');
 
-FB.setAccessToken('CAACEdEose0cBAE0GtZC7BHlKNkn6MDnxZAQZArACAs5oVZCFIaeZAYUj9oYaWCgpSF4W6jgx17KUZATJc1GTWycPzAcubSjBJEBHxvELxIDTf9WNvTOzLvB75ZBZAbbOncAETZCbA21Fp6BDeXPoRZBs2pP05ZACOOKHKggJMtZBEd4FUpCFSSYpQ6KaATlOIYnSZASI461ZCmjq6SJK1eSmZCUgSNb');
+FB.setAccessToken('CAACEdEose0cBACZAbXHt7toKzjCRPbv5hZBwjmtwusfxd7R2MTsWlXY8i5CLtr9lCHfN6dqJvI8hsRBg6wVDfTtbkZCNnsGjMGgrrBmPqrtack4THMpyv8vMrfz7baJns0U8NwfmbHqVZAcoTKP4vuiLb4WIYE8MNi7nRtysipzwyoQwAlexaAfZA3XJrLdfifZAJCxuWClRXUYBzzB73ZA');
 
-FB.api('/v2.3/me/home', function (err, data) {
+/*
+FB.api('oauth/access_token', {
+    client_id: '998701320140541',
+    client_secret: '2e711148de356026d7c0e967462cf5e5',
+    grant_type: 'client_credentials'
+}, function (res) {
+    if(!res || res.error) {
+        console.log(!res ? 'error occurred' : res.error);
+        return;
+    }
+
+    var accessToken = res.access_token;
+	FB.setAccessToken(res.access_token);
+});*/
+
+FB.api('/v2.3/me', function (err, data) {
 	if(err) {
 		console.log(err);
 		return;
 	}
 
-	var parsed = JSON.parse(data);
+	
+	/*var parsed = JSON.parse(data);
 	
 	fs.writeFile('message.txt', data, function (err) {
 		if (err) {
 			console.log(err);
 		}
 		console.log('It\'s saved!');
-	});
+	});*/
 	
 	//console.log(parse);
-	//console.log(data);
+	console.log(data);
 });
 
 
--- thesis/routes/index.js
+++ thesis/routes/index.js
@@ -4,10 +4,16 @@ var router = express.Router();
 //Added these
 var FB = require('fb');
 var url = require('url');
+var fs = require('fs');
+
+//Set global access_token
+FB.setAccessToken('CAACEdEose0cBACmRxwDdJVu56nDSOAJfVK3Ec8kJ0BjjdmaVYmZA0vUcU8FOyC7z1nxn9SWsZAdYwBsCujXj0TnGb6i8FG5GRAAfishSGZAII4SuADCtyudY3GaZCBREKFennqlZBXh4440LQJ8g04hzdV1EefdlS4oApK9ClOtqQMRqUtXgcLcFuRSvgTyoVD7eyFZBNhOGJ2vcbO1X2O');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	res.render('index', { title: 'Express' });
+	
+	
 });
 
 /* POST to Add User Service */
@@ -17,14 +23,56 @@ router.post('/adduser', function(req, res) {
     var userName = req.body.Email;
     var userPass = req.body.Password;
 
+	
     // Set our collection
     //var collection = db.get('usercollection');
-	//console.log(userPass);
+	console.log(userName);
+	console.log(userPass);
+	
+	//Assume we have the access token
+	//Only can pull 25 feed items apparently
+	FB.api('/v2.3/me/home' , function (response) {
+	
+		if (response && !response.error) {
+			//console.log(response);
+			
+			console.log(typeof response.data[0].message);
+			
+			var size = response.data.length;
+			console.log("Size of data is", response.data.length);
+			var i = 0;
+				
+			try {
+				JSON.parse(response[0]);
+			} catch (e) {
+				console.log("not JSON");
+			}
+			
+			//Need to append all fields
+			while (i < size) {
+				//var parsed = JSON.parse(response[i]);
+				var data = i.toString() + " " + response.data[i].name;
+				//if (data != "undefined") {
+					//fs.writeFile('./log/log.JSON', data + "\r\n", function (err) {
+					fs.appendFile('./log/log.txt', data + "\r\n", function (err) {
+					if (err) {
+						return console.log(err);
+					}
+					});
+				//}
+				//console.log(i);
+				i++;
+			}
+		} else {
+			//Print out error message
+			console.log(response);
+		}
+	});
 	/*
 	FB.api('oauth/access_token', {
 		client_id: '998701320140541',
 		client_secret: '2e711148de356026d7c0e967462cf5e5',
-		redirect_uri: 'http://localhost:3000/callback',
+		redirect_uri: 'http://localhost:3000/',
 		code: 'code'
 	}, function (res) {
 		if(!res || res.error) {
@@ -33,7 +81,7 @@ router.post('/adduser', function(req, res) {
 		}
 
 		var accessToken = res.access_token;
-		var expires = res.expires ? res.expires : 0;
+		//var expires = res.expires ? res.expires : 0;
 	});*/
 	
 	//This way requires an access token
